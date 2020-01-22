4ff16cbc09358338ad2aa58a621237c9be3cb3ef
KhmylkoPN <khmulko@gmail.com>
2020-01-22 15:16:50 -0800

    Add views + identity

---
+++ BlogWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BlogWebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlogWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace BlogWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- BlogWebApp/Controllers/HomeController.cs
+++ BlogWebApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using BlogWebApp.Models;
 using Core.Interfaces;
 using BLayer.DTO;
 using System.Collections.Generic;
+using System;
 
 namespace BlogWebApp.Controllers
 {
@@ -23,16 +24,165 @@ namespace BlogWebApp.Controllers
 
         public IActionResult Index()
         {
-            var articlesDTO = _articleService.GetAll();
+            List<ArticleViewModel> articleViewModels = new List<ArticleViewModel>();
 
-            var articleViewModels = new List<ArticleViewModel>();
-
-            foreach(var articleDTO in articlesDTO)
+            try
             {
-                articleViewModels.Add(_articleMapper.Map(articleDTO));
+                var articlesDTO = _articleService.GetAll();
+                articleViewModels = new List<ArticleViewModel>();
+
+                foreach (var articleDTO in articlesDTO)
+                {
+                    articleViewModels.Add(_articleMapper.Map(articleDTO));
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message, "Stopped program because of exception");
             }
 
-            return View();
+            return View(articleViewModels);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id.HasValue)
+            {
+                try
+                {
+                    var articleDTO = _articleService.GetById(id.Value);
+
+                    if (articleDTO != null)
+                    {
+                        var articleViewModel = _articleMapper.Map(articleDTO);
+                        return View(articleViewModel);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message, "Stopped program because of exception");
+                    throw;
+                }
+
+                return NotFound();
+            }
+            else
+            {
+                return View(new ArticleViewModel());
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ArticleViewModel article)
+        {
+            if (article.Id != 0)
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        var articleDTO = _articleMapper.Map(article);
+                        _articleService.Edit(articleDTO);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex.Message, "Stopped program because of exception");
+                        throw;
+                    }
+                }
+                else
+                    return View(article);
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        var articleDTO = _articleMapper.Map(article);
+                        _articleService.Add(articleDTO);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex.Message, "Stopped program because of exception");
+                        throw;
+                    }
+                }
+                else
+                    return View(article);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                try
+                {
+                    var articleDTO = _articleService.GetById(id.Value);
+
+                    if (articleDTO != null)
+                    {
+                        var articleViewModel = _articleMapper.Map(articleDTO);
+                        return View(articleViewModel);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message, "Stopped program because of exception");
+                    throw;
+                }
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int? id)
+        {
+            _logger.LogInformation($"{id}");
+
+            if (id != null)
+            {
+                try
+                {
+                    _articleService.Delete(id.Value);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message, "Stopped program because of exception");
+                    throw;
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id != null)
+            {
+                try
+                {
+                    var articleDTO = _articleService.GetById(id.Value);
+
+                    if (articleDTO != null)
+                    {
+                        var articleViewModel = _articleMapper.Map(articleDTO);
+                        return View(articleViewModel);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message, "Stopped program because of exception");
+                    throw;
+                }
+            }
+
+            return NotFound();
         }
 
         public IActionResult Privacy()
